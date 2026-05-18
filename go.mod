125737c3cfc132fee1f815d20c17967a69dcc44d
izzzzzi <izmailov9@gmail.com>
2026-05-19 00:41:26 +0500

    fix: keep browser control go122 compatible

--- go.mod
+++ go.mod
@@ -1,18 +1,21 @@
 module github.com/izzzzzi/agent-aget
 
-go 1.26
-
-require github.com/spf13/cobra v1.8.1
+go 1.22
 
 require (
-	github.com/chromedp/cdproto v0.0.0-20260321001828-e3e3800016bc // indirect
-	github.com/chromedp/chromedp v0.15.1 // indirect
-	github.com/chromedp/sysutil v1.1.0 // indirect
-	github.com/go-json-experiment/json v0.0.0-20260214004413-d219187c3433 // indirect
+	github.com/chromedp/chromedp v0.10.1
+	github.com/spf13/cobra v1.8.1
+)
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20241003230502-a4a8f7c660df // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.4.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
 )
--- internal/browser/launcher_unix_test.go
+++ internal/browser/launcher_unix_test.go
@@ -3,7 +3,9 @@
 package browser
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -16,7 +18,13 @@ func TestStopKillsSpawnedChildProcess(t *testing.T) {
 	dir := t.TempDir()
 	exe := filepath.Join(dir, "fake-browser")
 	childPIDPath := filepath.Join(dir, "child.pid")
-	script := "#!/bin/sh\nsleep 30 &\necho $! > " + strconv.Quote(childPIDPath) + "\nsleep 30\n"
+	helperLogPath := filepath.Join(dir, "helper.log")
+	testExe, err := os.Executable()
+	if err != nil {
+		t.Fatal(err)
+	}
+	script := "#!/bin/sh\nAGET_BROWSER_HELPER=1 AGET_CHILD_PID_PATH=" + strconv.Quote(childPIDPath) +
+		" exec " + strconv.Quote(testExe) + " -test.run=TestBrowserChildHelper -- > " + strconv.Quote(helperLogPath) + " 2>&1\n"
 	if err := os.WriteFile(exe, []byte(script), 0755); err != nil {
 		t.Fatal(err)
 	}
@@ -30,7 +38,7 @@ func TestStopKillsSpawnedChildProcess(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	childPID := readChildPID(t, childPIDPath)
+	childPID := readChildPID(t, childPIDPath, helperLogPath)
 	t.Cleanup(func() {
 		_ = syscall.Kill(childPID, syscall.SIGKILL)
 	})
@@ -49,9 +57,27 @@ func TestStopKillsSpawnedChildProcess(t *testing.T) {
 	t.Fatalf("child process %d still exists after Stop", childPID)
 }
 
-func readChildPID(t *testing.T, path string) int {
+func TestBrowserChildHelper(t *testing.T) {
+	if os.Getenv("AGET_BROWSER_HELPER") != "1" {
+		return
+	}
+
+	cmd := exec.Command("sleep", "30")
+	if err := cmd.Start(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+	if err := os.WriteFile(os.Getenv("AGET_CHILD_PID_PATH"), []byte(strconv.Itoa(cmd.Process.Pid)), 0600); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+	_ = cmd.Wait()
+	os.Exit(0)
+}
+
+func readChildPID(t *testing.T, path, logPath string) int {
 	t.Helper()
-	deadline := time.Now().Add(5 * time.Second)
+	deadline := time.Now().Add(30 * time.Second)
 	for time.Now().Before(deadline) {
 		body, err := os.ReadFile(path)
 		if err == nil {
@@ -65,6 +91,9 @@ func readChildPID(t *testing.T, path string) int {
 			t.Fatal(err)
 		}
 		time.Sleep(20 * time.Millisecond)
+	}
+	if body, err := os.ReadFile(logPath); err == nil && len(body) > 0 {
+		t.Logf("helper log:\n%s", body)
 	}
 	t.Fatalf("timed out waiting for child pid file %s", path)
 	return 0
